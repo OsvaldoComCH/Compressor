40705e16b7a314d8fbd76ecaea8c2f6ed320eaa1
OsvaldoComCH <eduhrsilva@gmail.com>
2024-08-06 16:50:42 -0300

    Correctly reading Files

--- Compressor.cpp
+++ Compressor.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
 
 int main(int argc, char ** argv)
 {
-    if(argc == 0){return;}
-    std::fstream File;
-    File.open(argv[0], std::fstream::in | std::fstream::out | std::fstream::app);
-    if(File.fail()){return;}
-    File << "Bom dia";
+    if(argc == 1){return 1;}
+    fstream File;
+    File.open(argv[1], fstream::in | fstream::out | fstream::app | fstream::binary);
+    File.unsetf(fstream::skipws);
+    if(File.fail()){return 1;}
+    if(argc > 2 && !std::strcmp(argv[2], "-d"))
+    {
+        std::cout << "-d\n";
+        File.close();
+    }else
+    {
+        vector<char> Buffer ((istreambuf_iterator<char>(File)), (istreambuf_iterator<char>()));
+        File.close();
+        for(int i = 0; i < Buffer.size(); ++i)
+        {
+            std::cout << Buffer[i];
+            Buffer[i] ^= 0xC7;
+        }
+        for(int i = 0; i < Buffer.size(); ++i)
+        {
+            std::cout << Buffer[i];
+        }
+    }
 }
